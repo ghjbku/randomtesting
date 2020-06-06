efc134e3e79ec29ec56606d42c26eac089977e48
Balázs Tóth <gawnoris@gmail.com>
2020-06-06 16:04:28 +0200

    file_stuff

---
+++ src/main/java/Main.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static String home;
+    static List<Character> drives = new ArrayList<>();
+
+    private static String getPathToSetFile() {
+        System.out.println("\ngetpath function:");
+        //this will get the users home, for example(C:/users/"UsersName"/)
+        home = System.getProperty("user.home");
+        File file = new File(System.getProperty("user.home"));
+        System.out.println(file.getParent());
+        File fil4 = new File(file.getParentFile().getParent());
+        System.out.println("fil4 " + fil4.toString());
+        return home; // returns the string that we just got(the path)
+    }
+
+    private static void check_op_system() {
+        System.out.println(getPathToSetFile() + "\n\ncheck_op_system method:");
+        String home_temp = System.getProperty("os.name");
+        if (home_temp.startsWith("W")) {
+            System.out.println("system name: " + home_temp);
+            use_Drive();
+            what_drives();
+        } else {
+            System.out.println("its not windows\n" + home_temp);
+
+        }
+    }
+
+    private static File check_drive(String drive_Name) {
+        File file = new File(drive_Name + ":\\");
+        if (file.exists()) {
+            return file;
+        } else {
+            System.out.println(drive_Name + " does not exist!");
+            return null;
+        }
+    }
+
+    private static void use_Drive() {
+        if (check_drive("D") == null) {
+        } else {
+            drives.add('D');
+        }
+        if (check_drive("E") == null) {
+        } else {
+            drives.add('E');
+        }
+        if (check_drive("F") == null) {
+        } else {
+            drives.add('F');
+        }
+        if (check_drive("H") == null) {
+        } else {
+            drives.add('H');
+        }
+        if (check_drive("I") == null) {
+        } else {
+            drives.add('I');
+        }
+    }
+
+    private static void what_drives() {
+        int iterator=0;
+        System.out.println("\nDrives:");
+        for (char i : drives) {
+            System.out.println(iterator+". "+i);
+            iterator++;
+        }
+    }
+
+    private static void look_up_files(char drive_name){
+        File file = new File(drive_name + ":\\");
+        File[] files = file.listFiles();
+
+        assert files != null;
+        for (File file_ : files) {
+            System.out.println(file_.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        check_op_system();
+
+        for (char i:drives) {
+            System.out.println("\nDrive "+i+":\n");
+            look_up_files(i);
+        }
+    }
+}
